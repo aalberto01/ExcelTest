44a85aa25825a240e1475f2db0e5299ea0db5c56
Aaron Lemus <aaron.lemus@cit.hn>
2020-09-08 08:11:53 -0600

    Add project files.

---
+++ Pages/ExcelBase.cs
@@ -0,0 +1,36 @@
+﻿using BlazorInputFile;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelTest.Pages
+{
+    public class ExcelBase : ComponentBase
+    {
+        protected string status;
+        protected MemoryStream ms;
+
+        protected async Task HandleSelection(IFileListEntry[] files)
+        {
+
+            var file = files.FirstOrDefault();
+            if (file != null)
+            {
+                // Just load into .NET memory to show it can be done
+                // Alternatively it could be saved to disk, or parsed in memory, or similar
+                ms = new MemoryStream();
+                await file.Data.CopyToAsync(ms);
+
+
+                status = $"Finished loading {file.Size} bytes from {file.Name}";
+            }
+        }
+
+
+    }
+}
+
---
+++ Shared/ExcelTableBase.cs
@@ -0,0 +1,50 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ExcelTest.Shared
+{
+    public class ExcelTableBase : ComponentBase
+    {
+
+        public MemoryStream _fileMemoryStream;
+
+        [Parameter]
+        public MemoryStream FileMemoryStream
+        {
+            get { return _fileMemoryStream; }
+            set { _fileMemoryStream = value; }
+        }
+
+
+        protected override void OnInitialized()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var reader = ExcelReaderFactory.CreateCsvReader(FileMemoryStream))
+            {
+                // Choose one of either 1 or 2:
+
+                // 1. Use the reader methods
+                do
+                {
+                    
+                    while (reader.Read())
+                    {
+                        // reader.GetDouble(0);
+                    }
+                } while (reader.NextResult());
+
+                // 2. Use the AsDataSet extension method
+                var result = reader;
+
+                // The result of each spreadsheet is in result.Tables
+            }
+        }
+    }
+}
